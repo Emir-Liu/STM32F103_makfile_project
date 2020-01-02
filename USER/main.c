822cade784423387bb97de9a1fe973d3d4a9c11b
Emir-Liu <929555739@qq.com>
2020-01-02 11:01:20 +0800

    1

--- USER/main.c
+++ USER/main.c
@@ -1,33 +1,64 @@
-#include "led.h"
-#include "delay.h"
-#include "sys.h"
-//ALIENTEK战舰STM32开发板实验1
-//跑马灯实验  
-//技术支持：www.openedv.com
-//广州市星翼电子科技有限公司
- int main(void)
- {		
-	GPIO_InitTypeDef  GPIO_InitStructure;
-	delay_init();	    	 //延时函数初始化	  
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB|RCC_APB2Periph_GPIOE, ENABLE);	 //使能PB,PE端口时钟
-	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;				 //LED0-->PB.5 端口配置
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		 //推挽输出
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		 //IO口速度为50MHz
-	GPIO_Init(GPIOB, &GPIO_InitStructure);					 //根据设定参数初始化GPIOB.5
-	GPIO_SetBits(GPIOB,GPIO_Pin_5);						 //PB.5 输出高
+#include "stm32f10x_conf.h"
+ 
+void uart_init(u32 bound){
+  GPIO_InitTypeDef GPIO_InitStructure;
+  USART_InitTypeDef USART_InitStructure;
+  NVIC_InitTypeDef NVIC_InitStructure;
 
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;	    		 //LED1-->PE.5 端口配置, 推挽输出
-	GPIO_Init(GPIOE, &GPIO_InitStructure);	  				 //推挽输出 ，IO口速度为50MHz
-	GPIO_SetBits(GPIOE,GPIO_Pin_5); 						 //PE.5 输出高 		 
-	while(1)
-	{
-		//GPIO_ResetBits(GPIOB,GPIO_Pin_5);
-		//GPIO_SetBits(GPIOE,GPIO_Pin_5);
-		//delay_ms(500);
-		GPIO_SetBits(GPIOB,GPIO_Pin_5);
-		//GPIO_ResetBits(GPIOE,GPIO_Pin_5);
-		//delay_ms(500);
-	}
- }
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1|RCC_APB2Periph_GPIOA, ENABLE); 
+
+  //USART1_TX   GPIOA.9
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9; //PA.9
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP; 
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  //USART1_RX   GPIOA.10
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;//PA10
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  //Usart1 NVIC 
+   NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=2 ;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;    
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;     
+  NVIC_Init(&NVIC_InitStructure); 
+
+  USART_InitStructure.USART_BaudRate = bound;
+  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+  USART_InitStructure.USART_StopBits = USART_StopBits_1;
+  USART_InitStructure.USART_Parity = USART_Parity_No;
+  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx; 
+  USART_Init(USART1, &USART_InitStructure); 
+  USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+  USART_Cmd(USART1, ENABLE);                    
+}
+ 
+int _write (int fd, char *pBuffer, int size)
+    {
+        int i=0;
+        for (i = 0; i < size; i++)
+        {
+            while (!(USART1->SR & USART_SR_TXE))
+            {
+            }
+            USART_SendData(USART1, pBuffer[i]);
+        }
+        return size;
+    }
+ 
+int main(void)
+{
+	uart_init(9600);
+    int i=0;
+ 
+  while(1)
+  {
+       for(i=0;i<1000000;i++);
+       printf("hello!\r\n");
+ 
+  }
+}
 
--- makefile
+++ makefile
@@ -1,17 +1,24 @@
-
+#输入工程名
 TARGET = test_project
 
+#arm-none-eabi工具链
 PREFIX	= arm-none-eabi-
 CC			= $(PREFIX)gcc
 AS			= $(PREFIX)as
 LD			= $(PREFIX)ld
 OBJCOPY	= $(PREFIX)objcopy
+#串口下载工具
+FLASH_DOWNLOAD = stm32flash
+#串口通信工具
+USART = minicom
 
 #读取当前工作目录
-TOP=$(shell pwd)
+TOP = $(shell pwd)
+#串口工作目录
+SERIAL=$(shell sudo find /dev -name 'ttyUSB*')
 
 #include头文件
-INC_FLAGS= -I $(TOP)/CORE                  	\
+INC_FLAGS= -I $(TOP)/CMSIS                 	\
            -I $(TOP)/HARDWARE    						\
            -I $(TOP)/STM32F10x_FWLib/inc  	\
            -I $(TOP)/SYSTEM        					\
@@ -25,9 +32,9 @@ INC_FLAGS= -I $(TOP)/CORE                  	\
 # -D STM32F10X_MD -D USE_STDPERIPH_DRIVER	:宏定义
 # -O0							:优化等级有O0,O1,O2等
 # -std=gnu11			:设定语言标准GNU11,当然可以换为C98等
-CFLAGS =  -W -Wall -g -mcpu=cortex-m3 -mthumb -D STM32F10X_MD -D USE_STDPERIPH_DRIVER $(INC_FLAGS) -O0 -std=gnu11
-ASFLAGS = -W -Wall -g -mcpu=cortex-m3 -mthumb 
-
+CFLAGS	=  -W -Wall -g -mcpu=cortex-m3 -mthumb -D STM32F10X_MD -D USE_STDPERIPH_DRIVER $(INC_FLAGS) -O0 -std=gnu11
+ASFLAGS	= -W -Wall -g -mcpu=cortex-m3 -mthumb 
+LDFLAGS	= -mthumb -mcpu=cortex-m3 -Wl,--start-group -lc -lm -Wl,--end-group -specs=nano.specs -specs=nosys.specs -static -Wl,-cref,-u,Reset_Handler -Wl,-Map=Project.map -Wl,--gc-sections -Wl,--defsym=malloc_getpagesize_P=0x80
 #搜索并返回当前路径下的所有.c文件的集合
 #将C_SRC包含的文件的.c后缀代替为.o文件
 C_SRC=$(shell find ./ -name '*.c')  
@@ -36,12 +43,14 @@ C_OBJ=$(C_SRC:%.c=%.o)
 ASM_SRC=$(shell find ./ -name '*.s')
 ASM_OBJ=$(ASM_SRC:%.s=%.o)
 
+LD_SRC=$(shell find ./ -name '*.ld')
+
 #.PHONY关键字，代表后面的执行对象，不是文件
-.PHONY: all clean update 
+.PHONY: all clean update flash usart test
 
 #all的依赖对象是$(C_OBJ)和$(ASM_OBJ),一个变量是以.c或.s为后缀文件替换为.o为后缀的文件的集合，如果.o文件修改则更新all。
-all:$(C_OBJ) $(ASM_OBJ)
-	$(CC) $(C_OBJ) $(ASM_OBJ) -T stm32_flash.ld -o $(TARGET).elf   -mthumb -mcpu=cortex-m3 -Wl,--start-group -lc -lm -Wl,--end-group -specs=nano.specs -specs=nosys.specs -static -Wl,-cref,-u,Reset_Handler -Wl,-Map=Project.map -Wl,--gc-sections -Wl,--defsym=malloc_getpagesize_P=0x80 
+all: $(C_OBJ) $(ASM_OBJ)
+	$(CC) $(C_OBJ) $(ASM_OBJ) -T $(LD_SRC) -o $(TARGET).elf $(LDFLAGS) 
 #通过.elf文件，生成.bin和.hex文件
 	$(OBJCOPY) $(TARGET).elf  $(TARGET).bin -Obinary 
 	$(OBJCOPY) $(TARGET).elf  $(TARGET).hex -Oihex
@@ -51,9 +60,7 @@ all:$(C_OBJ) $(ASM_OBJ)
 $(C_OBJ):%.o:%.c
 	$(CC) -c $(CFLAGS) -o $@ $<
 
-#$(ASM_OBJ):%.o:%.c
-#	$(AS) -c $(ASFLAGS) -o $@ $<
-$(ASM_OBJ): $(ASM_SRC)
+$(ASM_OBJ):%.o:%.s
 	$(AS) -c $(ASFLAGS) -o $@ $<
 
 clean:
@@ -67,8 +74,14 @@ clean:
 update:
 	openocd -f /usr/share/openocd/scripts/interface/stlink-v2.cfg  -f /usr/share/openocd/scripts/target/stm32f1x_stlink.cfg -c init -c halt -c "flash write_image erase $(TOP)/LED_project.hex" -c reset -c shutdown
  
+#通过flash下载程序时，需要将BOOT0拉高，BOOT1置0
+#同时，需要管理员权限来控制端口
+flash:
+	sudo $(FLASH_DOWNLOAD) $(SERIAL)
+	sudo $(FLASH_DOWNLOAD) -w $(TARGET).hex -v -g 0 $(SERIAL)
 
-
+usart:
+	sudo $(USART)
  
 
 
